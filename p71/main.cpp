810ebbc9a67d0e196f8c94a0f63c19728cc5a135
Yerassyl Zeinolla <yerassyl94@gmail.com>
2019-09-16 12:47:00 +0600

    p71

--- p71/main.cpp
+++ p71/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-vector<string> permutations(string s) {
-	
+vector<string> permutations(string s, int left, int right) {
+	vector<string> result;
+	vector<string> intermediate;
+	if (left == right) {
+		result.push_back(s);
+	}
+	else {
+		for (int i=left; i <= right; i++) {
+			swap(s[left],s[i]);
+			intermediate = permutations(s, left +1, right);
+			result.insert(result.end(), intermediate.begin(), intermediate.end());
+			swap(s[left],s[i]);
+		}
+	}
+	return result;
 }
 
 int main(){
-	string s = "abcdefg";
+	string s = "abcdef";
+	int n = s.size();
 	vector<string> result;
-	result = permutations(s);
-
-	for (int i=0; i< result.size(); i++){
+	result = permutations(s, 0, n-1);
+	
+	for(int i=0; i< result.size(); i++){
 		cout<<result[i]<<endl;
 	}
-	
 	return 0;
 }
